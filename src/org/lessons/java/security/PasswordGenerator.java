60e51a91b98d8acaea70ab8ede61d7f4120c4830
AndreaRispoli97 <Andrea9715@outlook.it>
2025-06-24 14:51:51 +0200

    Refactor PasswordGenerator to accept user input for name, surname, and color

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -4,12 +4,23 @@ import java.util.Scanner;
 
 public class PasswordGenerator {
     public static void main(String[] args) {
-        String name = "Andrea";
-        String surName = "Rispoli";
-        String color = "Rosso";
+        String name;
+        String surName;
+        String color;
         int dayOfBirth;
         int monthOfBirth;
         int ageOfBirth;
+
+        Scanner inputDate = new Scanner(System.in);
+
+        System.out.print("Inserisci il tuo nome ");
+        name = inputDate.nextLine();
+
+        System.out.print("Inserisci il tuo cognome ");
+        surName = inputDate.nextLine();
+
+        System.out.print("Inserisci il tuo colore ");
+        color = inputDate.nextLine();
 
         Scanner dateOfBirth = new Scanner(System.in);
 
@@ -22,6 +33,7 @@ public class PasswordGenerator {
         System.out.print("Inserisci l'anno di nascita ");
         ageOfBirth = dateOfBirth.nextInt();
 
+        inputDate.close();
         dateOfBirth.close();
 
         int datePass = dayOfBirth + monthOfBirth + ageOfBirth;
